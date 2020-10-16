594ca11679390891a28142da7d3fa2d2f3876066
Safiyy Kanjiyani <safiyykanjiyani@gmail.com>
2020-10-16 11:00:03 -0700

    TODO updated

--- videos.py
+++ videos.py
@@ -1,6 +1,16 @@
 '''
 TODO:
-1. Subtitle Scraper for Keywords
-2. Video Getter Based on Subtitles
-3. Video Splicer and Editor
+1. Get Scenes from Movies
+2. Get Video Based on Scene Selctions
+3. Splice Scenes from Movies + Compile as One
+
+PROCESS:
+Part 1.
+Option 1.
+1. Use audio desciptions of movies to describe what's going on
+2. Use voice recognition to turn that audio descriptions into text
+3. Now you have text files decribing movies so that ideally should help in determinign what scenes are about.
+
+Option 2.
+1. Use subtitles to determine what's happening in scene (via dialogue)
 '''
